6a0ac825e1edfabfee4bb933b4cc75558b76d687
Kelviny H <kelvinyhenrique17@gmail.com>
2020-06-24 18:33:54 -0300

    Building Header

--- src/screens/Home/index.js
+++ src/screens/Home/index.js
@@ -1,10 +1,13 @@
 import React, { useRef, useEffect } from 'react';
-import { Text, View, StatusBar, Button, Animated, Dimensions, Easing } from 'react-native';
+import { Text, View, StatusBar, Button, Animated, Dimensions, Easing, Image, TouchableHighlight, ScrollView } from 'react-native';
 import styles from './styles';
+import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome'
+import { faBell, faWallet, faMoneyBill, faBarcode, faSearchDollar } from '@fortawesome/free-solid-svg-icons'
+import profileImage from '../../assets/Images/profile.jpg';
 
 const screenHeight = Math.round(Dimensions.get('window').height);
 
-const thirty = (25 * screenHeight) / 100;
+const thirty = (30 * screenHeight) / 100;
 
 export default function Home() {
 
@@ -27,8 +30,56 @@ export default function Home() {
         <View style={styles.BoxContainerHome}>
             <StatusBar barStyle="light-content" backgroundColor={'#FF5400'} />
             <Animated.View style={[styles.BoxHeader, { height: fadeAnim }]}>
+                <View style={styles.BoxMenu}>
+                    <View style={styles.avaliableBalance}>
+                        <Text style={styles.balanceText}>R$5.598,50</Text>
+                        <Text style={styles.balanceSubtitle}>Valor Disponível</Text>
+                    </View>
+                    <View style={styles.profileOptions}>
+                        <FontAwesomeIcon icon={faBell} size={25} color={'#FFF'} />
+                        <Image source={profileImage} style={styles.ProfileImage} />
+                    </View>
+                </View>
+                <View style={styles.scrollAlignment} >
+                    <TouchableHighlight style={{alignItems:'center', justifyContent:'center'}} onPress={() => {alert('Clickou')}} underlayColor='#FF5400'>
+                        <View style={styles.boxButtonHeader}>
+                            <View style={styles.iconsHomeHeader}>
+                                <FontAwesomeIcon icon={faWallet} size={20} color={'#FF5400'} />
+                            </View>
+                            <Text style={{color:'#FFF', fontWeight:'bold', marginTop:5}}>Pagar</Text>
+                        </View>
+                    </TouchableHighlight>
+
+                    <TouchableHighlight style={{alignItems:'center', justifyContent:'center', padding:0}} onPress={() => {alert('Clickou')}} underlayColor='#FF5400'>
+                        <View style={styles.boxButtonHeader}>
+                            <View style={styles.iconsHomeHeader}>
+                                <FontAwesomeIcon icon={faMoneyBill} size={27} color={'#FF5400'} />
+                            </View>
+                            <Text style={{color:'#FFF', fontWeight:'bold', marginTop:5}}>Receber</Text>
+                        </View>
+                    </TouchableHighlight>
+
+                    <TouchableHighlight style={{alignItems:'center', justifyContent:'center', padding:0}} onPress={() => {alert('Clickou')}} underlayColor='#FF5400'>
+                        <View style={styles.boxButtonHeader}>
+                            <View style={styles.iconsHomeHeader}>
+                                <FontAwesomeIcon icon={faBarcode} size={25} color={'#FF5400'} />
+                            </View>
+                            <Text style={{color:'#FFF', fontWeight:'bold', marginTop:5}}>Contas</Text>
+                        </View>
+                    </TouchableHighlight>
+
+                    <TouchableHighlight style={{alignItems:'center', justifyContent:'center', padding:0}} onPress={() => {alert('Clickou')}} underlayColor='#FF5400'>
+                        <View style={styles.boxButtonHeader}>
+                            <View style={styles.iconsHomeHeader}>
+                                <FontAwesomeIcon icon={faSearchDollar} size={20} color={'#FF5400'} />
+                            </View>
+                            <Text style={{color:'#FFF', fontWeight:'bold', marginTop:5}}>Gastos</Text>
+                        </View>
+                    </TouchableHighlight>
+                </View>
+
             </Animated.View>
-            <View style={styles.BoxTest}>
+            <View style={styles.BoxBottom}>
                 <View style={styles.BoxHistory}></View>
             </View>
 
--- src/screens/Home/styles.js
+++ src/screens/Home/styles.js
@@ -1,5 +1,4 @@
 import { StyleSheet, Dimensions } from 'react-native';
-
 export default StyleSheet.create({
     BoxContainerHome:{
         flex:1,
@@ -9,6 +8,17 @@ export default StyleSheet.create({
     BoxHeader:{
         backgroundColor:'#FF5400',
         width:'100%',
+        alignItems:'center',
+        justifyContent:'center'
+    },
+
+    BoxMenu:{
+        width:'100%',
+        flexDirection:'row',
+        alignItems:'center',
+        paddingLeft:30,
+        paddingRight:30,
+        justifyContent:'space-between'
     },
     BoxHistory: {
         backgroundColor:'#FFF',
@@ -17,8 +27,58 @@ export default StyleSheet.create({
         borderTopRightRadius:35,
         borderTopLeftRadius:35,
     },
-    BoxTest:{
+    BoxBottom:{
         backgroundColor:'#FF5400',
         height:'70%',
-    }
+    },
+    avaliableBalance:{
+        width:'70%',
+        alignItems:'flex-start'
+    },
+    balanceText: {
+        fontWeight:'bold',
+        color:'#FFF',
+        fontSize:35,
+        
+    },
+    balanceSubtitle:{
+        color:'#fff',
+        fontWeight:'bold'
+    },
+    profileOptions:{
+        width:'30%',
+        alignItems:'center',
+        flexDirection:'row',
+        justifyContent:'space-around'
+    },
+    ProfileImage:{
+        width:55,
+        height:55,
+        borderWidth:2,
+        borderColor:'#FFF',
+        borderRadius:15,
+    },
+    scrollAlignment:{
+        flexDirection:'row',
+        width:'100%',
+        height:90,
+        paddingLeft:30,
+        paddingRight:30,        
+        justifyContent:'space-around',
+        marginTop:10,
+    },
+
+    iconsHomeHeader:{
+        backgroundColor:'#FFF',
+        alignItems:'center',
+        justifyContent:'center',
+        width:60,
+        height:50,
+        borderRadius:15,
+        alignSelf:'center'
+    },
+    boxButtonHeader:{
+        alignItems:'center'
+    },  
+
 });
